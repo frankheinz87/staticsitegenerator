d0b8005f0099eabcf5f1d304e8d5237224d33868
frankheinz87 <frankheinz1987@gmail.com>
2025-06-11 21:13:09 +0200

    implemented docs folder for production usecase

--- src/main.py
+++ src/main.py
@@ -4,10 +4,16 @@ from pagegenerator import generate_page,generate_pages_recursive
 from fileprep import empty_destdir,file_preparation
 import os
 import shutil
+import sys
+
+if len(sys.argv)>1:
+    basepath=str(sys.argv[1])
+else:
+    basepath="/"
 
 def main():
-    file_preparation("static","public")
+    file_preparation("static","docs")
     #generate_page("content/index.md","template.html","public/index.html")
-    generate_pages_recursive("content","template.html","public")
+    generate_pages_recursive("content","template.html","docs",basepath)
 
 main()
--- src/pagegenerator.py
+++ src/pagegenerator.py
@@ -2,7 +2,8 @@ from mdtohtmlnodeconverter import markdown_to_html_node
 from titleextractor import extract_title
 import os
 
-def generate_page(from_path, template_path, dest_path):
+
+def generate_page(from_path, template_path, dest_path,basepath="/"):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         read_data=f.read()
@@ -12,13 +13,14 @@ def generate_page(from_path, template_path, dest_path):
     node=markdown_to_html_node(read_data)
     html=node.to_html()
     title=extract_title(read_data)
-    final_data=template_data.replace("{{ Title }}",title).replace("{{ Content }}", html)
+    final_data=template_data.replace("{{ Title }}",title).replace("{{ Content }}", html).replace('href="/',f'href="{basepath}').replace('src="/',f'src="{basepath}')
+
     #creating target file
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path,"w") as final:
         final.write(final_data)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path,basepath="/"):
     content_list=os.listdir(dir_path_content)
 
     if content_list==[]:
@@ -29,7 +31,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         dest_path=os.path.join(dest_dir_path,item)
         if os.path.isfile(content_path):
             html_dest_path=dest_path.rstrip(".md")+".html"
-            generate_page(content_path,template_path,html_dest_path)                  
+            generate_page(content_path,template_path,html_dest_path,basepath)                  
         else:
             generate_pages_recursive(content_path,template_path,dest_path)
     
